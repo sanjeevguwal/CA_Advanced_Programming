2b9cda16967e33031836750956ce613224ddf94b
sanjeevguwal <guwalsanjeev@gmail.com>
2023-04-14 17:45:12 +0100

    updated imgs

--- app2.py
+++ app2.py
@@ -8,7 +8,7 @@ app = Flask('__name__')
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'webauth'
+app.config['MYSQL_DB'] = 'flask_mysqldb1'
 
 
 mysql = MySQL(app)
