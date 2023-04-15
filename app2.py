d11187f3827e7c2157361c438004abd837c78d97
sanjeevguwal <guwalsanjeev@gmail.com>
2023-04-15 20:25:32 +0100

    update redirection and db name

--- app2.py
+++ app2.py
@@ -9,7 +9,7 @@ app = Flask('__name__')
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'webauth'
+app.config['MYSQL_DB'] = 'traveldb'
 
 
 mysql = MySQL(app)
